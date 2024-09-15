91a4850092ca7f4dd79dc3df375ec671eaa3d28f
glimadev07 <glimadev07@gmail.com>
2024-09-15 19:07:24 -0300

    refactor: removed unnecessary loading env from server.ts

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,6 @@
 import { createServer } from "./config/create-server";
 import { initPaymentRoutes } from "./presentation/routes/paymentRoutes";
-import dotenv from "dotenv";
 import envs from "./config/envs";
-
-dotenv.config();
 
 const server = createServer();
 
